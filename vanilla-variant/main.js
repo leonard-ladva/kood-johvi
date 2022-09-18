bf0bcfd709a799f106fc1422a6913b2898516137
Leonard Ladva <leonard.ladva@gmail.com>
2022-09-18 15:49:36 +0300

    backup

--- vanilla-variant/main.js
+++ vanilla-variant/main.js
@@ -1,25 +1,83 @@
+const UserID = '12'
+const matchesBox = document.querySelector('div.matches')
 // getAuthToken gets the API authentication token and returns it 
-function getAuthToken() {
-	const myheaders = new Headers()
-	myheaders.append('Content-Type', 'application/json')
-	
+async function getAuthToken() {
 	return fetch("https://intra.proekspert.ee/pulse-johvi/auth", {
-	method: 'POST',
-	headers: myheaders,
-	body: JSON.stringify({ username: 'user', password: 'user'})
-})
-.then(resp => resp.json())
-.then(data => {
-	return data.token
-})
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ username: 'user', password: 'user'})
+	})
+	.then(resp => resp.json())
+	.then(data => {
+		return data.token
+	})
 }
 
-getAuthToken().then(token => {
+// getCurrentUserSkill
+async function getCurrentUserSkills() {
+	const token = await getAuthToken()
+
+	return fetch(`https://intra.proekspert.ee/pulse-johvi/api/employees/${UserID}?expand=skills`, {
+		method: 'GET',
+		headers: {
+			'Authorization': `Bearer ${token}`, 
+			'Content-Type': 'application/json',
+		},
+	})
+	.then(resp => resp.json())
+	.then(data => {
+		const skills = []
+		for(let skill of data.skills) {
+			skills.push(skill.name)
+		}
+		return skills
+	})
+}
 
 
+// getProjects
+async function getAllProjects() {
+	const token = await getAuthToken()
 
+	return fetch(`https://intra.proekspert.ee/pulse-johvi/api/projects?expand=technologies`, {
+		method: 'GET',
+		headers: {
+			'Authorization': `Bearer ${token}`, 
+			'Content-Type': 'application/json',
+		},
+	})
+	.then(resp => resp.json())
+	.then(data => {
+		return data	
+	})
+}
 
+async function main() {
+	const matches = new Set()
 
+	const skills = await getCurrentUserSkills()
+	const projects = await getAllProjects()
+	console.log(skills)
 
-})
+	for(let project of projects) {
+		for(let technology of project.technologies) {
+			
+			console.log("TECH:", technology)
+			if(skills.includes(technology)) {
+				matches.add(project.name)
+			}
+		}	
+	}
+	console.log("Matches:", matches)
 
+	for(let match of matches) {
+		const project =  document.createElement('h1')
+		project.textContent = match
+		project.classList.add('MuiChip-root', 'jss91', 'MuiChip-outlined', 'jss92')
+
+		matchesBox.appendChild(project)
+	}
+}
+main()
